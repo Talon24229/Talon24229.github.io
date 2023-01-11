910eb548999aa39cf763f80f4ded0449d0bf5515
Ryan24313 <79584392+Ryan24313@users.noreply.github.com>
2023-01-11 11:34:29 -0500

    daf

---
+++ miniGames/functions.js
@@ -0,0 +1,35 @@
+function getData() {
+	return localStorage.getItem('gameData')
+}
+function sendData(variable) {
+	localStorage.setItem('gameData', JSON.stringify(variable))
+}
+function resetData() {
+	return {
+		LemonadeClown: {
+			normal: {
+				highScore: 0
+			},
+			endless: {
+				highScore: 0
+			},
+			timed: {
+				highScore: 0
+			}
+		},
+		fruitPunchClown: {
+			normal: {
+				highScore: 0
+			},
+			endless: {
+				highScore: 0
+			},
+			timed: {
+				highScore: 0
+			}
+		},
+		SLNight4: {
+			highScore: 0
+		}
+	}
+}
